c18ef2d6bf23d5e8302bfd32b616ab5f8a632b39
CamelinF <60093903+CamelinF@users.noreply.github.com>
2024-03-23 08:43:23 +0100

    Add files via upload

---
+++ Krimp/trunk/build/CMakeFiles/krimp.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/camelinf/LCS-papier/Krimp/trunk/FicConf.cpp" "CMakeFiles/krimp.dir/FicConf.cpp.o" "gcc" "CMakeFiles/krimp.dir/FicConf.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/FicMain.cpp" "CMakeFiles/krimp.dir/FicMain.cpp.o" "gcc" "CMakeFiles/krimp.dir/FicMain.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/algo/Algo.cpp" "CMakeFiles/krimp.dir/algo/Algo.cpp.o" "gcc" "CMakeFiles/krimp.dir/algo/Algo.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/algo/CTFile.cpp" "CMakeFiles/krimp.dir/algo/CTFile.cpp.o" "gcc" "CMakeFiles/krimp.dir/algo/CTFile.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/algo/CTISList.cpp" "CMakeFiles/krimp.dir/algo/CTISList.cpp.o" "gcc" "CMakeFiles/krimp.dir/algo/CTISList.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/algo/CTLog.cpp" "CMakeFiles/krimp.dir/algo/CTLog.cpp.o" "gcc" "CMakeFiles/krimp.dir/algo/CTLog.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/algo/CodeTable.cpp" "CMakeFiles/krimp.dir/algo/CodeTable.cpp.o" "gcc" "CMakeFiles/krimp.dir/algo/CodeTable.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/algo/coverfull/CFCodeTable.cpp" "CMakeFiles/krimp.dir/algo/coverfull/CFCodeTable.cpp.o" "gcc" "CMakeFiles/krimp.dir/algo/coverfull/CFCodeTable.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/algo/coverfull/CoverFull.cpp" "CMakeFiles/krimp.dir/algo/coverfull/CoverFull.cpp.o" "gcc" "CMakeFiles/krimp.dir/algo/coverfull/CoverFull.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/algo/coverfull/ParallelCoverFull.cpp" "CMakeFiles/krimp.dir/algo/coverfull/ParallelCoverFull.cpp.o" "gcc" "CMakeFiles/krimp.dir/algo/coverfull/ParallelCoverFull.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/algo/coverpartial/CCCPCodeTable.cpp" "CMakeFiles/krimp.dir/algo/coverpartial/CCCPCodeTable.cpp.o" "gcc" "CMakeFiles/krimp.dir/algo/coverpartial/CCCPCodeTable.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/algo/coverpartial/CCPCodeTable.cpp" "CMakeFiles/krimp.dir/algo/coverpartial/CCPCodeTable.cpp.o" "gcc" "CMakeFiles/krimp.dir/algo/coverpartial/CCPCodeTable.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/algo/coverpartial/CPACodeTable.cpp" "CMakeFiles/krimp.dir/algo/coverpartial/CPACodeTable.cpp.o" "gcc" "CMakeFiles/krimp.dir/algo/coverpartial/CPACodeTable.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/algo/coverpartial/CPCodeTable.cpp" "CMakeFiles/krimp.dir/algo/coverpartial/CPCodeTable.cpp.o" "gcc" "CMakeFiles/krimp.dir/algo/coverpartial/CPCodeTable.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/algo/coverpartial/CPOCodeTable.cpp" "CMakeFiles/krimp.dir/algo/coverpartial/CPOCodeTable.cpp.o" "gcc" "CMakeFiles/krimp.dir/algo/coverpartial/CPOCodeTable.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/algo/coverpartial/CPROCodeTable.cpp" "CMakeFiles/krimp.dir/algo/coverpartial/CPROCodeTable.cpp.o" "gcc" "CMakeFiles/krimp.dir/algo/coverpartial/CPROCodeTable.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/algo/coverpartial/CoverPartial.cpp" "CMakeFiles/krimp.dir/algo/coverpartial/CoverPartial.cpp.o" "gcc" "CMakeFiles/krimp.dir/algo/coverpartial/CoverPartial.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/algo/coverpartial/ParallelCoverPartial.cpp" "CMakeFiles/krimp.dir/algo/coverpartial/ParallelCoverPartial.cpp.o" "gcc" "CMakeFiles/krimp.dir/algo/coverpartial/ParallelCoverPartial.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/blocks/classifier/Classifier.cpp" "CMakeFiles/krimp.dir/blocks/classifier/Classifier.cpp.o" "gcc" "CMakeFiles/krimp.dir/blocks/classifier/Classifier.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/blocks/classifier/CtAnalyser.cpp" "CMakeFiles/krimp.dir/blocks/classifier/CtAnalyser.cpp.o" "gcc" "CMakeFiles/krimp.dir/blocks/classifier/CtAnalyser.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/blocks/stream/Histogram.cpp" "CMakeFiles/krimp.dir/blocks/stream/Histogram.cpp.o" "gcc" "CMakeFiles/krimp.dir/blocks/stream/Histogram.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/blocks/stream/StreamAlgo.cpp" "CMakeFiles/krimp.dir/blocks/stream/StreamAlgo.cpp.o" "gcc" "CMakeFiles/krimp.dir/blocks/stream/StreamAlgo.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/ficmkii.cpp" "CMakeFiles/krimp.dir/ficmkii.cpp.o" "gcc" "CMakeFiles/krimp.dir/ficmkii.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/tasks/ClassifyTH.cpp" "CMakeFiles/krimp.dir/tasks/ClassifyTH.cpp.o" "gcc" "CMakeFiles/krimp.dir/tasks/ClassifyTH.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/tasks/DataTransformTH.cpp" "CMakeFiles/krimp.dir/tasks/DataTransformTH.cpp.o" "gcc" "CMakeFiles/krimp.dir/tasks/DataTransformTH.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/tasks/DissimilarityTH.cpp" "CMakeFiles/krimp.dir/tasks/DissimilarityTH.cpp.o" "gcc" "CMakeFiles/krimp.dir/tasks/DissimilarityTH.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/tasks/MainTH.cpp" "CMakeFiles/krimp.dir/tasks/MainTH.cpp.o" "gcc" "CMakeFiles/krimp.dir/tasks/MainTH.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/tasks/MineTH.cpp" "CMakeFiles/krimp.dir/tasks/MineTH.cpp.o" "gcc" "CMakeFiles/krimp.dir/tasks/MineTH.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/tasks/StreamTH.cpp" "CMakeFiles/krimp.dir/tasks/StreamTH.cpp.o" "gcc" "CMakeFiles/krimp.dir/tasks/StreamTH.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/tasks/TaskHandler.cpp" "CMakeFiles/krimp.dir/tasks/TaskHandler.cpp.o" "gcc" "CMakeFiles/krimp.dir/tasks/TaskHandler.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Krimp/trunk/build/CMakeFiles/krimp.dir/algo/CTLog.cpp.o.d
@@ -0,0 +1,5 @@
+CMakeFiles/krimp.dir/algo/CTLog.cpp.o: \
+ /home/camelinf/LCS-papier/Krimp/trunk/algo/CTLog.cpp \
+ /usr/include/stdc-predef.h \
+ /home/camelinf/LCS-papier/Krimp/trunk/LocalFic.h \
+ /home/camelinf/LCS-papier/Krimp/trunk/blocks/Blocks.h

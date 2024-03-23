4c1998f868d2c11fe5ff7abe2e3c25ab9ca96f7e
CamelinF <60093903+CamelinF@users.noreply.github.com>
2024-03-23 08:36:42 +0100

    Add files via upload

---
+++ Krimp/trunk/build/bass/CMakeFiles/Bass.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/camelinf/LCS-papier/Krimp/trunk/bass/Bass.cpp" "bass/CMakeFiles/Bass.dir/Bass.cpp.o" "gcc" "bass/CMakeFiles/Bass.dir/Bass.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/bass/db/ClassedDatabase.cpp" "bass/CMakeFiles/Bass.dir/db/ClassedDatabase.cpp.o" "gcc" "bass/CMakeFiles/Bass.dir/db/ClassedDatabase.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/bass/db/Database.cpp" "bass/CMakeFiles/Bass.dir/db/Database.cpp.o" "gcc" "bass/CMakeFiles/Bass.dir/db/Database.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/bass/db/DbAnalyser.cpp" "bass/CMakeFiles/Bass.dir/db/DbAnalyser.cpp.o" "gcc" "bass/CMakeFiles/Bass.dir/db/DbAnalyser.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/bass/db/DbFile.cpp" "bass/CMakeFiles/Bass.dir/db/DbFile.cpp.o" "gcc" "bass/CMakeFiles/Bass.dir/db/DbFile.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/bass/db/FicDbFile.cpp" "bass/CMakeFiles/Bass.dir/db/FicDbFile.cpp.o" "gcc" "bass/CMakeFiles/Bass.dir/db/FicDbFile.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/bass/db/FimiDbFile.cpp" "bass/CMakeFiles/Bass.dir/db/FimiDbFile.cpp.o" "gcc" "bass/CMakeFiles/Bass.dir/db/FimiDbFile.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/bass/isc/BinaryFicIscFile.cpp" "bass/CMakeFiles/Bass.dir/isc/BinaryFicIscFile.cpp.o" "gcc" "bass/CMakeFiles/Bass.dir/isc/BinaryFicIscFile.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/bass/isc/BinaryFicIscFileAsync.cpp" "bass/CMakeFiles/Bass.dir/isc/BinaryFicIscFileAsync.cpp.o" "gcc" "bass/CMakeFiles/Bass.dir/isc/BinaryFicIscFileAsync.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/bass/isc/FicIscFile.cpp" "bass/CMakeFiles/Bass.dir/isc/FicIscFile.cpp.o" "gcc" "bass/CMakeFiles/Bass.dir/isc/FicIscFile.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/bass/isc/FicIscFileAsync.cpp" "bass/CMakeFiles/Bass.dir/isc/FicIscFileAsync.cpp.o" "gcc" "bass/CMakeFiles/Bass.dir/isc/FicIscFileAsync.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/bass/isc/FimiIscFile.cpp" "bass/CMakeFiles/Bass.dir/isc/FimiIscFile.cpp.o" "gcc" "bass/CMakeFiles/Bass.dir/isc/FimiIscFile.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/bass/isc/IscFile.cpp" "bass/CMakeFiles/Bass.dir/isc/IscFile.cpp.o" "gcc" "bass/CMakeFiles/Bass.dir/isc/IscFile.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/bass/isc/ItemSetCollection.cpp" "bass/CMakeFiles/Bass.dir/isc/ItemSetCollection.cpp.o" "gcc" "bass/CMakeFiles/Bass.dir/isc/ItemSetCollection.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/bass/isc/ItemTranslator.cpp" "bass/CMakeFiles/Bass.dir/isc/ItemTranslator.cpp.o" "gcc" "bass/CMakeFiles/Bass.dir/isc/ItemTranslator.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/bass/itemstructs/BAI32CoverSet.cpp" "bass/CMakeFiles/Bass.dir/itemstructs/BAI32CoverSet.cpp.o" "gcc" "bass/CMakeFiles/Bass.dir/itemstructs/BAI32CoverSet.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/bass/itemstructs/BAI32ItemSet.cpp" "bass/CMakeFiles/Bass.dir/itemstructs/BAI32ItemSet.cpp.o" "gcc" "bass/CMakeFiles/Bass.dir/itemstructs/BAI32ItemSet.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/bass/itemstructs/BM128CoverSet.cpp" "bass/CMakeFiles/Bass.dir/itemstructs/BM128CoverSet.cpp.o" "gcc" "bass/CMakeFiles/Bass.dir/itemstructs/BM128CoverSet.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/bass/itemstructs/BM128ItemSet.cpp" "bass/CMakeFiles/Bass.dir/itemstructs/BM128ItemSet.cpp.o" "gcc" "bass/CMakeFiles/Bass.dir/itemstructs/BM128ItemSet.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/bass/itemstructs/CoverNode.cpp" "bass/CMakeFiles/Bass.dir/itemstructs/CoverNode.cpp.o" "gcc" "bass/CMakeFiles/Bass.dir/itemstructs/CoverNode.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/bass/itemstructs/CoverSet.cpp" "bass/CMakeFiles/Bass.dir/itemstructs/CoverSet.cpp.o" "gcc" "bass/CMakeFiles/Bass.dir/itemstructs/CoverSet.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/bass/itemstructs/ItemSet.cpp" "bass/CMakeFiles/Bass.dir/itemstructs/ItemSet.cpp.o" "gcc" "bass/CMakeFiles/Bass.dir/itemstructs/ItemSet.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/bass/itemstructs/Mask.cpp" "bass/CMakeFiles/Bass.dir/itemstructs/Mask.cpp.o" "gcc" "bass/CMakeFiles/Bass.dir/itemstructs/Mask.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/bass/itemstructs/Uint16CoverSet.cpp" "bass/CMakeFiles/Bass.dir/itemstructs/Uint16CoverSet.cpp.o" "gcc" "bass/CMakeFiles/Bass.dir/itemstructs/Uint16CoverSet.cpp.o.d"
+  "/home/camelinf/LCS-papier/Krimp/trunk/bass/itemstructs/Uint16ItemSet.cpp" "bass/CMakeFiles/Bass.dir/itemstructs/Uint16ItemSet.cpp.o" "gcc" "bass/CMakeFiles/Bass.dir/itemstructs/Uint16ItemSet.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
